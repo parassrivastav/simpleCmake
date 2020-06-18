ebad80488258c38a014ac03f79323278b80a8d97
Paras Srivastav <paras.sri50@gmail.com>
2020-06-18 14:25:05 +0530

    body position

---
+++ main1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<animals/mule.h>
+#include<animals/horse.h>
+#include<animals/funcs.h>
+#include<reactphysics3d/reactphysics3d.h>
+
+using namespace std;
+
+int main() {
+  mule m;
+  horse h;
+  cout << "\nMain is running.";
+  dumbFunc();
+  m.displayMule();
+  h.displayHorse();
+  cout << endl;
+}
