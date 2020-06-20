514786ebdae2bce5e53734f32d4fdc97a362c82b
parassrivastav <paras.sri50@gmail.com>
2020-06-20 19:03:42 +0530

    cma

---
+++ CMakeFiles/animalSim.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/main.cpp" "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/CMakeFiles/animalSim.dir/main.cpp.o"
+  "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/src/animals/funcs.cpp" "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/CMakeFiles/animalSim.dir/src/animals/funcs.cpp.o"
+  "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/src/animals/horse.cpp" "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/CMakeFiles/animalSim.dir/src/animals/horse.cpp.o"
+  "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/src/animals/mule.cpp" "/home/sarabjot/Downloads/website/osg/vcpkg/buildtrees/osg/src/raph-3.6.4-7f0089ee03/paras/simpleCmake/CMakeFiles/animalSim.dir/src/animals/mule.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/animalSim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/animalSim.dir/main.cpp.o"
+  "CMakeFiles/animalSim.dir/src/animals/funcs.cpp.o"
+  "CMakeFiles/animalSim.dir/src/animals/horse.cpp.o"
+  "CMakeFiles/animalSim.dir/src/animals/mule.cpp.o"
+  "animalSim.pdb"
+  "animalSim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/animalSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
